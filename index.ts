69e1a6ea931904e3a37e9efa218dda6af8bbdb18
Shravan Hariharan <shravanhariharan2@gmail.com>
2022-01-27 11:47:21 -0800

    Add comments in index.ts

--- index.ts
+++ index.ts
@@ -3,11 +3,14 @@ import router from './controllers';
 
 const server = express();
 
+// allow routes to parse JSON from the request
 server.use(express.urlencoded({ extended: true }));
 server.use(express.json());
 
+// specify prefix for the routes implemented in controllers.ts
 server.use('/', router);
 
+// start the server
 server.listen(3000, () => {
   console.log('Server started on port 3000');
 });
