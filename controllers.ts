e408d1fa66f618b7f2c2b838baa1c0514801f20c
Shravan Hariharan <shravanhariharan2@gmail.com>
2022-02-01 19:26:47 -0800

    Update solution code correctness

--- controllers.ts
+++ controllers.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import uuid from 'uuid';
+import * as uuid from 'uuid';
 import { Auction, Bid, User } from './types';
 
 const router = express.Router();
@@ -9,23 +9,34 @@ const auctions = new Map<string, Auction>();
 const bids = new Map<string, Bid>();
 const users = new Map<string, User>();
 
+router.get('/auctions', (req, res) => {
+  const auctionList = Array.from(auctions.values());
+  return res.status(200).json({ auctions: auctionList });
+});
+
 router.get('/auction/:id', (req, res) => {
   const id: string = req.params.id;
   const auction = auctions.get(id);
   if (auction) {
-    res.status(200).json({ auction });
+    return res.status(200).json({ auction });
   } else {
-    res.status(404).json({ error: `Auction ${id} not found` });
+    return res.status(404).json({ error: `Auction ${id} not found` });
   }
 });
 
 router.post('/auction', (req, res) => {
   const auction: Auction = req.body.auction;
 
+  // convert strings to dates
+  auction.start = new Date(auction.start);
+  auction.end = new Date(auction.end);
+
+  // create auction
   const id = uuid.v4();
   auction.id = id;
   auctions.set(id, auction);
-  res.status(200).json({ auction });
+
+  return res.status(200).json({ auction });
 });
 
 router.post('/auction/:id/bid', (req, res) => {
@@ -33,11 +44,13 @@ router.post('/auction/:id/bid', (req, res) => {
   const auction = auctions.get(auctionId);
   // check if auction id is valid
   if (!auction) {
-    res.status(404).json({ error: `Auction ${auctionId} not found` });
+    return res.status(404).json({ error: `Auction ${auctionId} not found` });
   }
-  // check if auction hasn't ended
-  if (new Date() > auction.end) {
-    res.status(400).json({ error: `Auction ${auctionId} has already ended!` });
+  // check if auction is ongoing
+  const now = new Date();
+  const isOngoing = now >= auction.start && now <= auction.end;
+  if (!isOngoing) {
+    return res.status(400).json({ error: `Auction ${auctionId} is not currently running!` });
   }
 
   const bid: Bid = req.body.bid;
@@ -46,30 +59,35 @@ router.post('/auction/:id/bid', (req, res) => {
 
   // check if user id is valid
   if (!user) {
-    res.status(404).json({ error: `User ${bidderId} not found` });
+    return res.status(404).json({ error: `User ${bidderId} not found` });
   }
   // check if user has enough balance to place this bid
   if (user.balance < bid.amount) {
-    res.status(400).json({ error: `User ${bidderId} cannot afford this bid!` });
+    return res.status(400).json({ error: `User ${bidderId} cannot afford this bid!` });
   }
 
-  const { currentBid, minIncrement } = auction;
-  const minimumBid = currentBid.amount + minIncrement;
-
   // check if bid amount is high enough
+  const { currentBid, minIncrement } = auction;
+  let minimumBid = 0;
+  if (!currentBid) {
+    minimumBid = auction.startingPrice + minIncrement;
+  } else {
+    minimumBid = currentBid.amount + minIncrement;
+  }
   if (bid.amount < minimumBid) {
-    res.status(400).json({ error: `Bid amount too low! Minimum bid must be: ${minimumBid}`})
+    return res.status(400).json({ error: `Bid amount too low! Minimum bid must be: ${minimumBid}`})
   }
 
   // update our database
   auction.currentBid = bid;
+  bid.auctionId = auction.id;
   auctions.set(auctionId, auction);
   const bidId = uuid.v4();
   bid.id = bidId;
   bids.set(bidId, bid);
 
   // return success
-  res.status(200).json({ bid });
+  return res.status(200).json({ bid });
 });
 
 router.post('/user', (req, res) => {
@@ -77,7 +95,7 @@ router.post('/user', (req, res) => {
   const id = uuid.v4();
   user.id = id;
   users.set(id, user);
-  res.status(200).json({ user });
+  return res.status(200).json({ user });
 });
 
 
--- types.ts
+++ types.ts
@@ -1,31 +1,30 @@
 export type Auction = {
   id?: string;
-  uuid: string;
   auctioneer: string;
   name: string;
   description: string;
   start: Date;
   end: Date;
-  startingBid: number;
+  startingPrice: number;
   currentBid: Bid;
   minIncrement: number;	// the minimum difference between the highest existing bid and an incoming bid
 }
 
 export type Bid = {
-  id: string;
-  auctionId: string;
+  id?: string;
+  auctionId?: string;
   bidder: string;
   amount: number;
 }
 
 export type User = {
-  id: string;
+  id?: string;
   username: string;
   balance: number;
 }
 
 export type Item = {
-  id: string;
+  id?: string;
   owner: string;
   value: number;
 }
